db1afe3fecf29429b36f289009539abdf181f1b8
yalem-source <yalemyee@gmail.com>
2025-05-19 11:56:02 -0400

    gh-pages

--- NetFlix-Project/src/Components/Rows/RowLists/RowList.jsx
+++ NetFlix-Project/src/Components/Rows/RowLists/RowList.jsx
@@ -1,24 +1,25 @@
+
 import React from 'react'
 import Row from "../Row/Row.jsx"
-import requests from "../../../Utils/Requests.jsx"
+import requests from "../../../Utils/Requests.jsx" // ✅ lowercase
 
 const RowList = () => {
- return (
-   <>
-     <Row
-       title="NETFLIX ORIGINALS"
-       fetchUrl={requests.fetchNetflixOriginals}
-       isLargeRow={true}
-     />
-     <Row title="Top Rated" fetchUrl={requests.fetchTopRatedMovies} />
-     <Row title="Action Movies" fetchUrl={requests.fetchActionsMovies} />
-     <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
-     <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
-     <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
-     <Row title="Tv Shows" fetchUrl={requests.fetchTvShow} />
-     <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
-   </>
- );
-}
+  return (
+    <>
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow={true}
+      />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="TV Shows" fetchUrl={requests.fetchTVShows} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+    </>
+  );
+};
 
-export default RowList
+export default RowList;
