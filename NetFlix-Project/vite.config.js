d3d41900b8096923f64fcd9754c24e65a348c1fe
yalem-source <yalemyee@gmail.com>
2025-05-19 12:31:16 -0400

    edit

--- NetFlix-Project/vite.config.js
+++ NetFlix-Project/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/2025-netflix-project",
+  base:"/2025-NetFlix-Project/",
 })
